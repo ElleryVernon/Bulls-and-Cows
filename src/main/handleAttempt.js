917a5b62067bd84dc3a361d76da29ceee134780f
ElleryVernon <tmpks3@gmail.com>
2023-08-19 22:23:26 +0900

    Refactor handleAttempt function to include size parameter and update comments

--- src/main/handleAttempt.js
+++ src/main/handleAttempt.js
@@ -5,24 +5,25 @@ import getGuess from "../prompts/getGuess.js";
  * 사용자의 추측을 처리하고 결과를 출력합니다.
  * @param {number} attempts 현재까지의 시도 횟수
  * @param {string} answer 정답
+ * @param {number} [size=3] 정답의 길이 (선택적 파라미터, 기본값은 3)
  * @return {boolean} 정답을 맞췄거나 사용자가 게임을 종료한 경우 true, 그렇지 않으면 false
  */
-const handleAttempt = (attempts, answer) => {
+const handleAttempt = (attempts, answer, size = 3) => {
 	const guess = getGuess();
 
 	if (!guess) {
-		return true;
+		return true; // 게임 종료
 	}
 
 	const { strike, ball } = countStrikeAndBall(guess, answer);
 
-	if (strike === 3) {
+	if (strike === size) {
 		alert(`축하합니다!! 정답은 ${answer}이고, ${attempts}번만에 맞추셨어요!`);
-		return true;
+		return true; // 정답 맞춤
 	}
 
 	alert(`스크라이크: ${strike}번, 볼: ${ball}번`);
-	return false;
+	return false; // 계속 진행
 };
 
 export default handleAttempt;
--- src/validation/isValidGuess.js
+++ src/validation/isValidGuess.js
@@ -1,15 +1,16 @@
 /**
  * 사용자의 입력이 세 자리 숫자이며, 각 자리의 숫자가 중복되지 않는지 확인합니다.
  * @param {string} input 사용자의 입력
+ * @param {number} [size=3] 정답의 길이 (선택적 파라미터, 기본값은 3)
  * @return {boolean} 입력이 세 자리 숫자이며, 중복된 숫자가 없는지 여부
  */
-const isValidGuess = (input) => {
-	if (input?.length !== 3 || isNaN(input)) {
+const isValidGuess = (input, size = 3) => {
+	if (input?.length !== size || isNaN(input)) {
 		return false;
 	}
 
 	const uniqueDigits = new Set(input);
-	return uniqueDigits.size === 3;
+	return uniqueDigits.size === size;
 };
 
 export default isValidGuess;
