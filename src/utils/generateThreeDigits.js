ddf6028d3df71d50072422d2ff7c2b7b92714a57
ElleryVernon <tmpks3@gmail.com>
2023-08-19 22:31:09 +0900

    Refactor handleAttempt function to include size parameter and update comments

--- src/utils/generateThreeDigits.js
+++ src/utils/generateThreeDigits.js
@@ -1,9 +1,9 @@
 /**
  * 0부터 9까지의 숫자를 무작위로 섞어 3자리 문자열로 만듭니다.
- * @return {string} 세 자리 문자열
+ * @return {string} 세 자리 숫자 문자열
  */
 const generateThreeDigits = () =>
-	Array.from({ length: 10 }, (_, idx) => idx)
+	[0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 		.sort(() => Math.random() - 0.5)
 		.slice(0, 3)
 		.join("");
